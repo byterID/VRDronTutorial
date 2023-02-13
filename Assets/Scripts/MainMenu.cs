afa401f400413fac833d80c4e4e0ef5ad01de71f
byterID <artemijgladkov44644@gmail.com>
2023-02-13 14:48:17 +0500

    Packages & Player Started

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    
+    //UI Elements
+    public GameObject Main;
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
